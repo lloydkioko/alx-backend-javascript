0715e46731db453383c4e0b10f0382d26e4a4ce9
lloyd kioko <ch.abene254@gmail.com>
2023-10-03 15:10:23 +0300

    initial comit

--- 0x06-unittests_in_js/3-payment.js
+++
@@ -1,6 +0,0 @@
-const Utils = require('./utils');
-
-function sendPaymentRequestToApi(totalAmount, totalShipping) {
-  sum = Utils.calculateNumber('SUM', totalAmount, totalShipping);
-  console.log(`The total is ${sum}`);
-};
